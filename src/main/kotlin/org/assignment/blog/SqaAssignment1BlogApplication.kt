1383e15d0c5ccaf1533f420f21063e54805b08d3
NathanW95 <nathan.williams@ada.ac.uk>
2025-12-02 12:24:01 +0000

    Initialize Empty Spring Boot Kotlin Project

---
+++ src/main/kotlin/org/assignment/blog/SqaAssignment1BlogApplication.kt
@@ -0,0 +1,11 @@
+package org.assignment.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SqaAssignment1BlogApplication
+
+fun main(args: Array<String>) {
+    runApplication<SqaAssignment1BlogApplication>(*args)
+}
---
+++ src/test/kotlin/org/assignment/blog/SqaAssignment1BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.assignment.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SqaAssignment1BlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
